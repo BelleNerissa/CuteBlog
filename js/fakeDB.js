679d1344dabc762bcb598c4c3cc331cb9ee87a96
BelleNerissa <49173787+BelleNerissa@users.noreply.github.com>
2019-06-19 20:00:15 -0300

    Update fakeDB.js

--- js/fakeDB.js
+++ js/fakeDB.js
@@ -1,9 +1,9 @@
 const fakeDB = [
     {
-      titulo: "TituloTeste1",
+      titulo: "Você sente vontade de apertar e morder coisas fofas? Entenda o porquê!",
       data: new Date(),
       curtidas: 0,
-      texto: "Lorem ipsum vitae lacus risus rhoncus aliquam velit at dapibus, hendrerit tortor sem eros ligula leo tellus netus, ipsum sit non ante ad tincidunt odio consectetur. sit volutpat risus feugiat aenean class tempor massa, torquent consequat lacus tellus quam euismod ipsum mauris, etiam fames nam facilisis aenean rutrum. hac lorem odio nisi lacus mauris et pellentesque massa tortor quisque ad cubilia commodo, molestie mollis nec praesent lacus vestibulum curabitur nullam metus semper elementum netus. dui dapibus felis varius tempor neque tellus diam commodo donec, inceptos sollicitudin vestibulum rutrum urna nam porttitor nunc purus, mauris neque pellentesque class tellus orci odio dictum. Proin tristique donec nunc eleifend aenean vel sollicitudin tincidunt dolor bibendum, torquent dapibus eget fames ad orci per taciti quam pharetra diam, etiam integer fames congue sociosqu laoreet aptent praesent feugiat. semper rhoncus lacinia auctor mollis eros auctor lobortis urna, ad sodales habitant tellus tempor mollis maecenas conubia, consequat iaculis potenti eros non phasellus convallis. lacinia per iaculis elit nibh tempus, tempor ullamcorper inceptos eleifend, fames posuere aenean hendrerit. integer aliquam pretium posuere dictum justo dictum mi, vel pellentesque lorem faucibus maecenas accumsan, velit egestas torquent tortor himenaeos egestas. lobortis ad diam lacinia elit tellus, cras vitae tortor nostra. ",
+      texto: "Quem nunca sentiu vontade de esmagar, apertar e morder os animazinhos ou mesmo crianças fofas? Todos esses impulsos são chamados de cute aggression, ou melhor, agressão fofa. Este comportamento é tão comum que está sendo estudado por algumas das melhores universidades do mundo. Há alguns anos, a Universidade de Yale tornou “cute agression” um termo acadêmico, comprovando que pessoas expostas a imagens fofíssimas de animais estouram mais plástico-bolha do que as que observam fotos corriqueiras. Mas, como explicar esse fenomeno? Em um estudo recém-publicado pela Universidade da Califórnia os pesquisadores observaram como o cérebro reage quando alguém sente a tal agressão. O estudo contou com 54 voluntários, onde eles foram expostos a imagens fofas (qualquer filhote de mamífero) e imagens não-fofas (algo neutro quanto à fofura). A reação cerebral deles era monitorada a todo instante. Após os exames, os pesquisadores concluíram que a reação da agressão à fofice está bastante associada a dois fatores: de um estímulo muito grande ao sistema de recompensa do cérebro ou seja, alguém que sente muito prazer em observar objeto fofo e uma sobrecarga do sistema que processa emoções. Quanto menor era o prazer que alguém sentia ao ver um bebê ou um cãozinho fofo, menor a chance de sentir a tal agressividade. Cerca de 35% dos voluntários não sabia o que era a “vontade de apertar essa fofurinha”. Os demais, porém, já estavam bastante acostumados com o sentimento. Os resultados indicam, então, que a agressão fofa é uma resposta desenvolvida para dar uma pausa no cérebro. A sensação de overdose de fofura pode ser exatamente o que o cérebro sente. Outra explicação para tais instintos é que a fofura funciona como uma sobrecarrega dos “processadores” e impede que o cérebro do adulto funcione direito fazendo com que um mecanismo impeça essa uma possível pane do sistema cerebral. ",
       imagem: "https://placekitten.com/640/360",
       comentarios: [
         {
@@ -17,11 +17,11 @@ const fakeDB = [
       ]
     },
     {
-      titulo: "TituloTeste2",
+      titulo: "Em Londres é possivel fazer yoga rodeado de filhotes..",
       data: new Date(),
       curtidas: 0,
-      texto: "Lorem ipsum vitae lacus risus rhoncus aliquam velit at dapibus, hendrerit tortor sem eros ligula leo tellus netus, ipsum sit non ante ad tincidunt odio consectetur. sit volutpat risus feugiat aenean class tempor massa, torquent consequat lacus tellus quam euismod ipsum mauris, etiam fames nam facilisis aenean rutrum. hac lorem odio nisi lacus mauris et pellentesque massa tortor quisque ad cubilia commodo, molestie mollis nec praesent lacus vestibulum curabitur nullam metus semper elementum netus. dui dapibus felis varius tempor neque tellus diam commodo donec, inceptos sollicitudin vestibulum rutrum urna nam porttitor nunc purus, mauris neque pellentesque class tellus orci odio dictum. Proin tristique donec nunc eleifend aenean vel sollicitudin tincidunt dolor bibendum, torquent dapibus eget fames ad orci per taciti quam pharetra diam, etiam integer fames congue sociosqu laoreet aptent praesent feugiat. semper rhoncus lacinia auctor mollis eros auctor lobortis urna, ad sodales habitant tellus tempor mollis maecenas conubia, consequat iaculis potenti eros non phasellus convallis. lacinia per iaculis elit nibh tempus, tempor ullamcorper inceptos eleifend, fames posuere aenean hendrerit. integer aliquam pretium posuere dictum justo dictum mi, vel pellentesque lorem faucibus maecenas accumsan, velit egestas torquent tortor himenaeos egestas. lobortis ad diam lacinia elit tellus, cras vitae tortor nostra. ",
-      imagem: "https://placekitten.com/640/360",
+      texto: "Um clube de yoga em Londres usa uma das armas mais poderosas para aumentar o bem-estar de seus clientes: filhotes de cachorros, gatos e até coelhos. Os pets fazem companhia aos alunos durante as aulas e podem receber carinho à vontade. O Pets Yoga deixa os bichinhos livres durante as sessões para que andem entre as pessoas, brinquem ou, se preferirem, tirem uma soneca. A prática, além de aliviar o estresse dos humanos, auxilia no processo de socialização dos animais. Segundo o site da empresa, os filhotes são cedidos pelos donos, ONGs ou criadores licenciados. O clube vende as sessões pelo site. O ingresso de uma aula com filhotes de cachorro custa £35, aproximadamente R$170. Já com coelhos, na promoção, vale £25, cerca de R$121. ",
+      imagem:"https://i.ytimg.com/vi/-479SFi3mcQ/hqdefault.jpg",
       comentarios: [
         {
           nome: "Bruna",
@@ -34,11 +34,11 @@ const fakeDB = [
       ]
     },
     {
-      titulo: "TituloTeste3",
+      titulo: "Texto Carinhoso",
       data: new Date(),
       curtidas: 0,
-      texto: "Lorem ipsum vitae lacus risus rhoncus aliquam velit at dapibus, hendrerit tortor sem eros ligula leo tellus netus, ipsum sit non ante ad tincidunt odio consectetur. sit volutpat risus feugiat aenean class tempor massa, torquent consequat lacus tellus quam euismod ipsum mauris, etiam fames nam facilisis aenean rutrum. hac lorem odio nisi lacus mauris et pellentesque massa tortor quisque ad cubilia commodo, molestie mollis nec praesent lacus vestibulum curabitur nullam metus semper elementum netus. dui dapibus felis varius tempor neque tellus diam commodo donec, inceptos sollicitudin vestibulum rutrum urna nam porttitor nunc purus, mauris neque pellentesque class tellus orci odio dictum. Proin tristique donec nunc eleifend aenean vel sollicitudin tincidunt dolor bibendum, torquent dapibus eget fames ad orci per taciti quam pharetra diam, etiam integer fames congue sociosqu laoreet aptent praesent feugiat. semper rhoncus lacinia auctor mollis eros auctor lobortis urna, ad sodales habitant tellus tempor mollis maecenas conubia, consequat iaculis potenti eros non phasellus convallis. lacinia per iaculis elit nibh tempus, tempor ullamcorper inceptos eleifend, fames posuere aenean hendrerit. integer aliquam pretium posuere dictum justo dictum mi, vel pellentesque lorem faucibus maecenas accumsan, velit egestas torquent tortor himenaeos egestas. lobortis ad diam lacinia elit tellus, cras vitae tortor nostra.",
-      imagem: "https://placekitten.com/640/360",
+      texto: " Tenho também meu lado ingênuo, carinhoso e aventureiro. Sou amante dos sonhos e refém dos meus desejos. As vezes ciumenta, apegada e carente.Logo mais Independente e quando necessário, indiferente. Responsável e sem juízo. Talvez já seja quase um caso perdido.. As vezes sou silêncio e solidão. Em outro instante quero gente na volta, adrenalina e emoção. Hora sou fogo e logo mais tempestade. Sou brisa suave e chuva de verão. ~ Sheila Kolberg  " ,
+      imagem:"https://pbs.twimg.com/media/DSuo1BrW0AIEu0d.jpg",
       comentarios: [
         {
           nome: "Bruna",
@@ -50,11 +50,11 @@ const fakeDB = [
         }
       ]
     },{
-      titulo: "TituloTeste4",
+      titulo: "Como trabalhar de voluntário com ursos Pandas...",
       data: new Date(),
       curtidas: 0,
-      texto: "Lorem ipsum vitae lacus risus rhoncus aliquam velit at dapibus, hendrerit tortor sem eros ligula leo tellus netus, ipsum sit non ante ad tincidunt odio consectetur. sit volutpat risus feugiat aenean class tempor massa, torquent consequat lacus tellus quam euismod ipsum mauris, etiam fames nam facilisis aenean rutrum. hac lorem odio nisi lacus mauris et pellentesque massa tortor quisque ad cubilia commodo, molestie mollis nec praesent lacus vestibulum curabitur nullam metus semper elementum netus. dui dapibus felis varius tempor neque tellus diam commodo donec, inceptos sollicitudin vestibulum rutrum urna nam porttitor nunc purus, mauris neque pellentesque class tellus orci odio dictum. Proin tristique donec nunc eleifend aenean vel sollicitudin tincidunt dolor bibendum, torquent dapibus eget fames ad orci per taciti quam pharetra diam, etiam integer fames congue sociosqu laoreet aptent praesent feugiat. semper rhoncus lacinia auctor mollis eros auctor lobortis urna, ad sodales habitant tellus tempor mollis maecenas conubia, consequat iaculis potenti eros non phasellus convallis. lacinia per iaculis elit nibh tempus, tempor ullamcorper inceptos eleifend, fames posuere aenean hendrerit. integer aliquam pretium posuere dictum justo dictum mi, vel pellentesque lorem faucibus maecenas accumsan, velit egestas torquent tortor himenaeos egestas. lobortis ad diam lacinia elit tellus, cras vitae tortor nostra.",
-      imagem: "https://placekitten.com/640/360",
+      texto: "Digamos que os ursos pandas são as criaturas mais amadas do planeta. Quantas vezes você já ficou impressionado com uma foto de um bebê panda ou passou horas vendo aqueles vídeos fofinhos das trapalhadas deles. Tudo bem que uma simples visita a um zoológico pode satisfazer a sua curiosidade em vê-los de pertinho, mas a realidade é  que estes dóceis animais estão em risco de extinção e realmente precisam de ajuda para sobreviver.Neste sentido, os centros de conservação chineses tem ajudado muito no cuidado com os pandas, especialmente depois que a Base de Chengdu abriu sua central em 1987 com apenas 6 pandas resgatados da vida selvagem e conseguiu aumentar consideravelmente a população dos pandinhas locais. E foi graças a este centro de conservação e aos voluntários que dedicam seu tempo no cuidado com os pandinhas que a espécie continua viva e muito bem de saúde.Então se você gosta de pandas e quer dar uma mãozinha aos chineses para a preservação desta espécie encontre um programa de voluntariado que precise de ajuda e mãos à obra.",
+      imagem:"https://pbs.twimg.com/profile_images/687376792970199040/TMbxjv_X_400x400.jpg",
       comentarios: [
         {
           nome: "Bruna",
@@ -66,11 +66,11 @@ const fakeDB = [
         }
       ]
     },{
-      titulo: "TituloTeste5",
+      titulo: "Como Ser Fofo??",
       data: new Date(),
       curtidas: 0,
-      texto: "Lorem ipsum vitae lacus risus rhoncus aliquam velit at dapibus, hendrerit tortor sem eros ligula leo tellus netus, ipsum sit non ante ad tincidunt odio consectetur. sit volutpat risus feugiat aenean class tempor massa, torquent consequat lacus tellus quam euismod ipsum mauris, etiam fames nam facilisis aenean rutrum. hac lorem odio nisi lacus mauris et pellentesque massa tortor quisque ad cubilia commodo, molestie mollis nec praesent lacus vestibulum curabitur nullam metus semper elementum netus. dui dapibus felis varius tempor neque tellus diam commodo donec, inceptos sollicitudin vestibulum rutrum urna nam porttitor nunc purus, mauris neque pellentesque class tellus orci odio dictum. Proin tristique donec nunc eleifend aenean vel sollicitudin tincidunt dolor bibendum, torquent dapibus eget fames ad orci per taciti quam pharetra diam, etiam integer fames congue sociosqu laoreet aptent praesent feugiat. semper rhoncus lacinia auctor mollis eros auctor lobortis urna, ad sodales habitant tellus tempor mollis maecenas conubia, consequat iaculis potenti eros non phasellus convallis. lacinia per iaculis elit nibh tempus, tempor ullamcorper inceptos eleifend, fames posuere aenean hendrerit. integer aliquam pretium posuere dictum justo dictum mi, vel pellentesque lorem faucibus maecenas accumsan, velit egestas torquent tortor himenaeos egestas. lobortis ad diam lacinia elit tellus, cras vitae tortor nostra.",
-      imagem: "https://placekitten.com/640/360",
+      texto: "Ser fofo é algo relacionado à demonstração de sua natureza gentil, amigável e inocente, projetando-a em todos os lugares. Pessoas gentis são autênticas, legais, quentes e possuem muitos amigos. Elas sabem como compartilhar esse calor com o mundo. Se estiver procurando mais maneiras de ser fofo ou de melhorar sua fofura, apenas siga estas fáceis dicas. Seja positivo. Pessoas fofas são sempre divertidas e espalham a alegria da vida para outros. O calor de uma pessoa fofa deve conseguir preencher um cômodo, sendo sentido a quilômetros de distância.Seja recatado. A timidez não é algo ruim. Ela dá um ar misterioso. Se você for tímido, porém abordável, pessoas continuarão fazendo perguntas e desejarão conhecê-lo melhor. Pessoas tímidas, ainda que doces e conversadeiras, são absolutamente adoráveis. Lembre-se de manter um clima positivo e amigável enquanto mantém o recato. Seja amigável. Pessoas fofas podem ser um pouco tímidas, mas elas sempre são amigáveis com aqueles que elas conhecem e com quem querem conhecer. Pessoas fofas são abordáveis, sendo que os outros sempre desejarão conhecê-las. Eis como preservar a fofura enquanto se é amigável.",
+      imagem:"https://i.pinimg.com/originals/d3/0e/76/d30e767b0735b5efb97548ba1585c121.jpg",
       comentarios: [
         {
           nome: "Bruna",
